c8b769768751a6bfa453071ddf37ed7952f9c3fd
meta-kritika-pandey4 <52685752+meta-kritika-pandey4@users.noreply.github.com>
2019-09-08 14:08:49 +0530

    Update BaseDao.java

--- dao/BaseDao.java
+++ dao/BaseDao.java
@@ -6,16 +6,15 @@ import java.util.List;
 import com.metacube.get2019.model.*;
 
 /**
- * 
  * @author Kritika
  *
  */
 public interface BaseDao {
-
-	List<Fruit>  getAllFruits(String query);
+	
+	List<Fruit> getAllFruits(String query);
 	Fruit getFruitByName(String query, String fruitName);
 	boolean updateFruit(String query);
 	boolean deleteAllFruits(String query);
-	boolean deleteFruitByName(String query);
-	
+	boolean deleteFruitByName(String query, String fruitName);
+	boolean updateFruit(String query, Fruit fruit);
 }
