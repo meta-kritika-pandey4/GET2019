58db862f9e43f5c54a5c37539258d8d93527c7f3
meta-kritika-pandey4 <52685752+meta-kritika-pandey4@users.noreply.github.com>
2019-09-23 12:19:44 +0530

    Update ProductFacade.java

--- EAD-Assignment-05/src/com/metacube/get2019/facade/ProductFacade.java
+++ EAD-Assignment-05/src/com/metacube/get2019/facade/ProductFacade.java
@@ -9,6 +9,7 @@ import com.metacube.get2019.enums.Status;
 import com.metacube.get2019.factory.Factory;
 import com.metacube.get2019.model.Cart;
 import com.metacube.get2019.model.Product;
+import com.metacube.get2019.util.Constant;
 
 /**
  * This class represents the facade layer
@@ -29,7 +30,7 @@ public class ProductFacade {
 	 * @return
 	 */
 	public boolean userAuthentication(String email, String password) {
-		String query = "select email from store_user where email = '" + email	+ "' and password = '" + password + "'";
+		String query =Constant.AUTHENTICATION_QUERY_1 + email	+ Constant.AUTHENTICATION_QUERY_2 + password + "'";
 		return productDao.userAuthentication(query);
 	}
 	
@@ -39,7 +40,7 @@ public class ProductFacade {
 	 * @return
 	 */
 	public int getUserIdByEmail(String email) {
-		String query = "select user_id from store_user where email='" + email + "'";
+		String query = Constant.GET_BY_EMAIL_QUERY + email + "'";
 		return productDao.getUserIdByEmail(query);
 	}
 	
@@ -49,7 +50,7 @@ public class ProductFacade {
 	 * @return
 	 */
 	public boolean isProductExist(int productCode) {
-		String query = "select product_code from product where product_code = '" + productCode + "'";
+		String query =Constant.PRODUCT_EXISTS+ productCode + "'";
 		return productDao.isProductExist(query);
 	}
 	
@@ -60,7 +61,7 @@ public class ProductFacade {
 	 * @return
 	 */
 	public boolean productIsInCart(int userId, int productCode) {
-		String query = "select product_code from cart where user_id ='"	+ userId + "' and product_code = '" + productCode + "'";
+		String query =Constant.PRODUCT_IN_CART_1+ userId +Constant.PRODUCT_IN_CART_2+ productCode + "'";
 		return productDao.productIsInCart(query);
 	}
 	
@@ -73,8 +74,8 @@ public class ProductFacade {
 	 */
 	public Status addProductToCart(int userId, int productCode,int productQuantity) {
 		List<String> queryList=new ArrayList<String>();
-		String query1 = "insert into cart values(" + productCode+ "," + productQuantity + ","+userId + ")";
-		String query2="update product set available_quantity=available_quantity-"+productQuantity+" where product_code="+productCode;
+		String query1 =Constant.INSERT_INTO_CART+ productCode+ "," + productQuantity + ","+userId + ")";
+		String query2=Constant.UPDATE_PRODUCT_1+productQuantity+Constant.UPDATE_PRODUCT_2+productCode;
 		queryList.add(query1);
 		queryList.add(query2);
 		return productDao.addProductToCart(queryList);
@@ -89,9 +90,9 @@ public class ProductFacade {
 	 */
 	public Status updateProductInCart(int userId, int productQuantity,Product productToUpdate) {
 		List<String> queryList=new ArrayList<String>();
-		String query1= "update cart set product_quantity ='" + productQuantity+ "' where user_id='" + userId + "' and product_code = '"+ productToUpdate.getProductCode() + "'";
-		String query2="select product_quantity from cart where product_code="+productToUpdate.getProductCode();
-		String query3="update product set available_quantity=available_quantity-("+productQuantity+"-?)";
+		String query1= Constant.UPDATE_CART_1 + productQuantity+Constant.UPDATE_CART_2 + userId +Constant.PRODUCT_IN_CART_2+ productToUpdate.getProductCode() + "'";
+		String query2=Constant.SELECT_PRODUCT_BY_PRODUCT_CODE+productToUpdate.getProductCode();
+		String query3=Constant.UPDATE_PRODUCT_1+productQuantity+"-?)";
 		queryList.add(query1);
 		queryList.add(query2);
 		queryList.add(query3);
@@ -104,8 +105,7 @@ public class ProductFacade {
 	 * @return
 	 */
 	public List<Cart> showCart(int userId) {
-		String query = "select c.product_code,p.product_name,p.product_type,c.product_quantity,p.price from cart c,product p where user_id='"
-				+ userId + "' and p.product_code=c.product_code";
+		String query =Constant.SHOW_CART_1 + userId + Constant.SHOW_CART_2;
 		return productDao.showCart(query);
 	}
 	
@@ -117,9 +117,9 @@ public class ProductFacade {
 	 */
 	public Status deleteProduct(int userId, Product productToDelete) {
 		List<String> queryList=new ArrayList<String>();
-		String query1 = "delete from cart where user_id = '" + userId+ "' and product_code = '" + productToDelete.getProductCode() + "'";
-		String query2="select product_quantity from cart where product_code="+productToDelete.getProductCode();
-		String query3="update product set available_quantity=available_quantity+?";
+		String query1 = Constant.DELETE_PRODUCT_FROM_CART_1+ userId+ Constant.PRODUCT_IN_CART_2 + productToDelete.getProductCode() + "'";
+		String query2=Constant.SELECT_PRODUCT_QUANTITY+productToDelete.getProductCode();
+		String query3=Constant.UPDATE_PRODUCT_3;
 		queryList.add(query1);
 		queryList.add(query2);
 		queryList.add(query3);
@@ -131,7 +131,7 @@ public class ProductFacade {
 	 * @return
 	 */
 	public List<Product> getProductList() {
-		String query = "select * from product";
+		String query = Constant.SELECT_ALL_PRODUCTS;
 		return productDao.getProductList(query);
 	}
 	
@@ -141,7 +141,7 @@ public class ProductFacade {
 	 * @return
 	 */
 	public Product getProductById(int productCode) {
-		String query="select * from product where product_code="+productCode;
+		String query=Constant.SELECT_ALL_PRODUCT_BY_PRODUCT_CODE+productCode;
 		return productDao.getProductById(query);
 	}
 }
