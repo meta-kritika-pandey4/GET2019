4fda1734d4d1f82bfc5dfe19d08e4a1b14205a2f
meta-kritika-pandey4 <52685752+meta-kritika-pandey4@users.noreply.github.com>
2019-09-23 07:58:41 +0530

    Assignment-EAD-05

---
+++ EAD-Assignment-05/src/com/metacube/get2019/enums/Status.java
@@ -0,0 +1,9 @@
+package com.metacube.get2019.enums;
+
+/**
+ * Enum to get status
+ * @author Kritika
+ */
+public enum Status {
+	ADDED, UPDATED, DELETED, FAILED;
+}
---
+++ EAD-Assignment-05/src/com/metacube/get2019/model/User.java
@@ -0,0 +1,30 @@
+package com.metacube.get2019.model;
+
+/**
+ * POJO class for user
+ * @author Kritika
+ *
+ */
+public class User {
+	int userId;
+	String email;
+	String password;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
